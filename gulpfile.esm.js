3697a782cddfc51ca5d1e77749ef06830cd800b7
goosy <goosy.jo@gmail.com>
2019-07-10 13:13:15 +0800

    fix copying without node_snap7.node file.

--- gulpfile.esm.js
+++ gulpfile.esm.js
@@ -6,13 +6,16 @@ import {
 } from 'gulp';
 import {tasks} from './gulpfile.user.js';
 
+task('copy-snap7', async () => {
+    src('node_modules/node-snap7/build/Release/node_snap7.node')
+        .pipe(dest('lib'));
+});
+
 task('copy', async () => {
     src('build-define/*').pipe(dest('dist/'));
     src('package.json').pipe(dest('dist/'));
     src('command/*').pipe(dest('dist/'));
     src('conf/*').pipe(dest('dist/conf'));
-    await src('node_modules/node-snap7/build/Release/node_snap7.node')
-        .pipe(dest('lib'));
     src('lib/*').pipe(dest('dist/lib/'));
 });
 
@@ -24,5 +27,5 @@ task('setproduction', async () => {
     process.env.PRODUCTION = true;
 })
 
-task('dev', series('setdev', ...tasks, 'copy'));
-task('build', series('setproduction', ...tasks, 'copy'));
+task('dev', series('setdev', 'copy-snap7', ...tasks, 'copy'));
+task('build', series('setproduction', 'copy-snap7', ...tasks, 'copy'));
