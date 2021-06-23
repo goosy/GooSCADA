086e6e39319968e4ac3a0de6d6cb1eb4b5214e95
goosy <goosy.jo@gmail.com>
2021-06-23 10:25:27 +0800

    fix on BCD.js

--- src/BCD.js
+++ src/BCD.js
@@ -3,7 +3,7 @@
  * @param {number} BCD_BYTE
  * @returns {number}
  */
-function BCD2DEC(BCD_BYTE) {
+export function BCD2DEC(BCD_BYTE) {
     if (BCD_BYTE > 0x99) return 99;
     return BCD_BYTE - (BCD_BYTE >> 4) * 6;
 }
@@ -12,18 +12,18 @@ function BCD2DEC(BCD_BYTE) {
  * @param {number} BCD_BYTE
  * @returns {number}
  */
-function DEC2BCD(DEC_BYTE) {
+export function DEC2BCD(DEC_BYTE) {
     if (DEC_BYTE > 99) return 0x99;
     return DEC_BYTE + Math.floor(DEC_BYTE / 10) * 6;
 }
 
 /**
- * 将数字转化为指定进制数组
+ * 将无符号数字按指定进制转化为字节数组
  * @param  {Number} num
  * @param  {Number} base
  * @return {number[]}
  */
-function Num2BaseArray(num, base) {
+export function Num2ByteArrayOfBase(num, base) {
     let ret = [];
     while (num > 0) {
         ret.unshift(num % base);
@@ -32,11 +32,11 @@ function Num2BaseArray(num, base) {
     return ret;
 }
 /**
- * 将无符号数字转化为字节数组
+ * 将无符号数字转化为字节数组（即256进制）
  * @param  {Number} num
  * @return {number[]}
  */
-function Num2ByteArray(num) {
+export function Num2ByteArray(num) {
     let ret = [];
     while (num != 0) {
         ret.unshift(num & 0xFF);
@@ -44,13 +44,22 @@ function Num2ByteArray(num) {
     }
     return ret;
 }
+
 /**
- * 将字节数组转化为指定进制的数字
+ * 将字节数组转化为指定字节进制的数字
  * @param {Uint8Array|Buffer} btye_list
  * @return {number}
  */
-function ByteArray2Number(btye_list, base) {
-    return btye_list.reduce((value, byte) => value * base + byte, 0);
+export function ByteArray2NumberByBase(byte_list, base) {
+    return byte_list.reduce((value, byte) => value * base + byte, 0);
+}
+/**
+ * 将字节数组转化为数字(即字节256进制)
+ * @param {Uint8Array|Buffer} btye_list
+ * @return {number}
+ */
+export function ByteArray2Number(byte_list) {
+    return byte_list.reduce((value, byte) => (value << 8) + byte, 0);
 }
 
 /**
@@ -60,14 +69,22 @@ function ByteArray2Number(btye_list, base) {
  */
 export function BCD2Decimal(BCD) {
     let DECArray = Num2ByteArray(BCD).map(BCD2DEC);
-    return ByteArray2Number(DECArray, 100);
+    return ByteArray2NumberByBase(DECArray, 100);
 }
 
 /**
+ * 将十进制数转换为BCD字节数组
  * @param {number} num
- * @return {Uint8Array}
+ * @return {number[]}
+ */
+export function Decimal2BCDArray(dec) {
+    return Num2ByteArrayOfBase(dec, 100).map(DEC2BCD);
+}
+
+/**
+ * @param {number} dec
+ * @return {number}
  */
 export function Decimal2BCD(dec) {
-    let BCDArray = Num2BaseArray(dec, 100).map(DEC2BCD);
-    return ByteArray2Number(BCDArray, 256);
+    return ByteArray2Number(Decimal2BCDArray(dec));
 }
