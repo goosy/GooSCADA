f95eb071a9b4e0453ffa8845e0f708e90de194b4
goosy <goosy.jo@gmail.com>
2021-06-30 07:12:42 +0800

    wscli add command: connect close fix some bugs

--- src/wscli.js
+++ src/wscli.js
@@ -7,64 +7,82 @@ import { createInterface } from "readline";
 let pingTimeout;
 function heartbeat() {
     console.log("heartbeat")
-    ws.alive = true;
+    websocket.alive = true;
     clearTimeout(pingTimeout);
     pingTimeout = setTimeout(() => {
-        ws.alive = false;
-        ws.close();
+        websocket.alive = false;
+        websocket.close();
     }, 30000 + 3000);
 }
 
-let ws = new WebSocket.Client('ws://127.0.0.1:8080');
-// ws.on('ping', heartbeat); // faye-websocket have not event:"ping"
-ws.on('close', function (event) {
-    clearTimeout(pingTimeout);
-    console.log('close', event.code, event.reason);
-    ws = null;
-});
-ws.on('message', function (event) {
-    console.log('message', event.data);
-    rl.prompt();
-    // ws.close();
-});
-ws.on('open', function (event) {
-    ws.alive = true;// heartbeat(); // faye-websocket have not event:"ping"
-});
+let websocket;
+function createConn(host) {
+    websocket?.close();
+    websocket = new WebSocket.Client(host);
+    // websocket.on('ping', heartbeat); // faye-websocket have not event:"ping"
+    websocket.on('close', function (event) {
+        clearTimeout(pingTimeout);
+        console.log('close', event.code, event.reason);
+        websocket = null;
+    });
+    websocket.on('message', function (event) {
+        console.log('message', event.data);
+        rl.prompt();
+        // websocket.close();
+    });
+    websocket.on('open', function (event) {
+        console.log('websocket connection created.')
+        websocket.alive = true;// heartbeat(); // faye-websocket have not event:"ping"
+    });
+}
+
+function send(action, name, value) {
+    if (!websocket?.alive) {
+        console.log('websocket have not connected. please connect first.');
+        return;
+    }
+    value = Number(value);
+    websocket.send(JSON.stringify({
+        action,
+        value,
+        name
+    }));
+}
 
 const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
     prompt: 'WS> '
 });
-
 rl.prompt();
 rl.on('line', (line) => {
-    line = line.trim()
-    switch (line) {
+    line = line.trim().split(/ +/);
+    let cmd = '', value, namepath;
+    switch (line[0]) {
         case 'help':
             console.log('help are not prepared.');
             break;
+        case 'connect':
+            createConn(line[1]);
+            break;
+        case 'close':
+            websocket?.close();
+            break;
+        case 'read':
+        case 'subscribe':
+            [cmd, ...namepath] = line;
+            break;
+        case 'write':
+            [cmd, value, ...namepath] = line;
+            break;
         case 'exit':
-            ws?.close();
+            websocket?.close();
             rl.close();
             break;
         default:
-            if (ws?.alive) {
-                let [cmd, value, ...name] = line.split(' ');
-                if (cmd === 'read' || cmd === 'subscribe') {
-                    name = [value, ...name];
-                    value = 0;
-                } else if (cmd !== 'write') break;
-                value = Number(value);
-                name = name.join('/');
-                ws.send(JSON.stringify({
-                    action: cmd,
-                    value,
-                    name
-                }));
-            }
             break;
     }
+    if (cmd !== '') send(cmd, namepath.join('/'), value);
     rl.prompt();
 })
 rl.on('close', () => {
