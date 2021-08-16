06bd9e7c5d9307b759094688ad2f1ec70c046154
goosy <goosy.jo@gmail.com>
2021-08-17 03:26:10 +0800

    update batch files

--- packages/ESD-JS-VPLC/commands/setup.cmd
+++ packages/ESD-JS-VPLC/commands/setup.cmd
@@ -1,9 +1,10 @@
 @echo off
 
-@REM where node > nul
-@REM if NOT %ERRORLEVEL% == 0 echo 没有安装Nodejs!!! && pause && exit
-@REM where pm2.cmd > nul
-@REM if NOT %ERRORLEVEL% == 0 call npm install -g pm2
+rem where node > nul
+rem if NOT %ERRORLEVEL% == 0 echo 没有安装Nodejs!!! && pause && exit
+
+rem where pm2.cmd > nul
+rem if NOT %ERRORLEVEL% == 0 call yarn global add pm2
 
 call yarn
 
